85c7fc490b39d411a9ead2ac56173b77b371db61
Florian Rudolf <rudolf@hs-ulm.de>
2020-05-22 23:00:55 +0200

    added vars to Layer

--- CNN/layer_dense.cpp
+++ CNN/layer_dense.cpp
@@ -1,6 +1,5 @@
 #include "layer_dense.h"
 
-Layer_Dense::Layer_Dense()
+Layer_Dense::Layer_Dense(uint32_t n_inputs, uint32_t n_neurons):n_inputs(n_inputs), n_neurons(n_neurons)
 {
-
 }
--- CNN/layer_dense.h
+++ CNN/layer_dense.h
@@ -1,11 +1,23 @@
+#include <iostream>
+#include <vector>
+
 #ifndef LAYER_DENSE_H
 #define LAYER_DENSE_H
 
+using namespace std;
 
 class Layer_Dense
 {
 public:
-    Layer_Dense();
+    Layer_Dense(uint32_t n_inputs, uint32_t n_neurons);
+
+    uint32_t n_inputs;
+    uint32_t n_neurons;
+
+private:
+    vector<vector<double>> weights;
+
+
 };
 
 #endif // LAYER_DENSE_H
