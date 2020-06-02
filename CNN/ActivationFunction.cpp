94f68db8a0847f678d0b5b7f3fa83a00f4a49d26
Florian Rudolf <rudolf@hs-ulm.de>
2020-06-02 13:44:00 +0200

    ActivationFunctions implemented (double version, not vector)

--- CNN/ActivationFunction.cpp
+++ CNN/ActivationFunction.cpp
@@ -1,2 +1,15 @@
 #include <ActivationFunction.h>
+#include <math.h>
 
+double ActivationFunction::Linear(double input){
+    return input;
+}
+
+double ActivationFunction::RectifiedLinear(double input){
+    if(input < 0) return 0;
+    return input;
+}
+
+double ActivationFunction::Sigmoid(double input){
+    return 1/(1+exp(input*-1));
+}
--- CNN/ActivationFunction.h
+++ CNN/ActivationFunction.h
@@ -3,6 +3,10 @@
 
 namespace ActivationFunction {
 
+    double RectifiedLinear(double input);
+    double Linear(double input); //identity
+    double Sigmoid(double input); //soft step
+
 }
 
 #endif // ACTIVATIONFUNCTION_H
